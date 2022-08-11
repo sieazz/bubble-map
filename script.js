9da1a5da0766169d21c2403af7b456dea4d26fe1
sieazz <sieazz@outlook.com>
2022-08-11 13:57:13 +0900

    Reinforce code to prevent saving graph without authorization

--- script.js
+++ script.js
@@ -95,18 +95,20 @@ fetch("./model/data.json", { mode: "no-cors" })
 
     // 비밀번호 입력받기
     function login(password){
-      var xhr = new XMLHttpRequest(); // new HttpRequest instance
-      xhr.open("POST", "/");
-      xhr.setRequestHeader("Content-Type", "text/plain");
-      xhr.responseType = 'text';
-      xhr.onload = () => {
-        if (xhr.readyState === xhr.DONE) {
-          if (xhr.status === 200) {
-            return JSON.parse(xhr.responseText);
+      return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest(); // new HttpRequest instance
+        xhr.open("POST", "/");
+        xhr.setRequestHeader("Content-Type", "text/plain");
+        xhr.responseType = 'text';
+        xhr.onload = () => {
+          if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200) {
+              resolve(JSON.parse(xhr.responseText));
+            }
           }
-        }
-      };
-      xhr.send(password);
+        };
+        xhr.send(password);
+      })
     }
 
     // data.json 업데이트
@@ -339,10 +341,14 @@ fetch("./model/data.json", { mode: "no-cors" })
 
         // 그래프 저장
         document.getElementById("updateGraph").onclick = function () {
-          let result = login(prompt("Enter password:"));
-          console.log(result);
-          updateData(cy);
-          closeEveryPopMenu();
+          login(prompt("Enter password:")).then( (result) => {
+            if (result) { 
+              updateData(cy); 
+            } else {
+              alert("Wrong password")
+            }
+            closeEveryPopMenu();
+          });
         };
       } else {
         if (e.target.isNode()) {
